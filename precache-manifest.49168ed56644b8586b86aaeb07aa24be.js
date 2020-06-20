75a10c57da30ab7388ea5849d828279d7aa48d5e
jcm005 <joseph.mattern@stonybrook.edu>
2020-06-19 21:07:33 -0400

    Updates

--- precache-manifest.49168ed56644b8586b86aaeb07aa24be.js
+++ precache-manifest.49168ed56644b8586b86aaeb07aa24be.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5e8c3dcbca2c6729405a2b1b39a33bcc",
+    "revision": "0ea0e3c2bcf59c4cf448c30a8b60ce7d",
     "url": "/home/index.html"
   },
   {
-    "revision": "68c80c3e6e2d181b64d9",
+    "revision": "2339f3d9410bfc8a23be",
     "url": "/home/static/css/main.126c14d6.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/home/static/js/2.faeddcdf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "68c80c3e6e2d181b64d9",
-    "url": "/home/static/js/main.15d889ed.chunk.js"
+    "revision": "2339f3d9410bfc8a23be",
+    "url": "/home/static/js/main.3d87596e.chunk.js"
   },
   {
     "revision": "b54ac55316918141f470",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.26201ace144199c98b1bb4801958bacc.js"
+  "/home/precache-manifest.49168ed56644b8586b86aaeb07aa24be.js"
 );
 
 self.addEventListener('message', (event) => {
